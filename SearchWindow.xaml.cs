4f416773608204aed2f1add6d9ab32e20b68b5d5
MikevanBreePXL <mike.vanbree@student.pxl.be>
2023-10-26 15:51:25 +0200

    [Update] - Added a search function

---
+++ SearchWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FlexibleCSVE
+{
+    /// <summary>
+    /// Interaction logic for SearchWindow.xaml
+    /// </summary>
+    public partial class SearchWindow : Window
+    {
+        Window mainWindow = Application.Current.MainWindow;
+
+        public SearchWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListBox contentListBox = (ListBox)mainWindow.FindName("contentListBox");
+
+            int startIndex = 0;
+            if (searchFromStartCheckBox.IsChecked == false)
+            {
+                startIndex = contentListBox.SelectedIndex + 1;
+            } else
+            {
+                searchFromStartCheckBox.IsChecked = false;
+                searchFromStartCheckBox.UpdateLayout();
+            }
+
+            for (int i = startIndex; i < contentListBox.Items.Count - 1; i++)
+            {
+                string entryLine = (string)contentListBox.Items.GetItemAt(i);
+                string searchText = searchTextBox.Text;
+
+                if (caseSensitiveCheckBox.IsChecked == false)
+                {
+                    entryLine = entryLine.ToLower();
+                    searchText = searchText.ToLower();
+                }
+
+                if (entryLine.Contains(searchText))
+                {
+                    contentListBox.SelectedIndex = i;
+                    contentListBox.ScrollIntoView(contentListBox.SelectedItem);
+                    return;
+                }
+            }
+
+            MessageBox.Show("The given value was not found");
+        }
+    }
+}
